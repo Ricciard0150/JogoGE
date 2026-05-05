5979f9207de610de88b61ccc0f053f9e1777af22
Ricciard0150 <richard.melo@alunos.sc.senac.br>
2026-05-05 11:16:50 -0300

    enemie

--- Assets/Assets/Scripts/Enemies/EnemieFollowing.cs
+++ Assets/Assets/Scripts/Enemies/EnemieFollowing.cs
@@ -1,12 +1,19 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements.Experimental;
 
 public class EnemieFollowing : MonoBehaviour
 {
     public Transform[] pontos;
+    public Transform player;
+
     private int pontoAtual = 0;
     private NavMeshAgent agent;
 
+    public float viewDistance = 10f;
+    public float viewAngle = 90f;
+
+    private bool chasing;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -15,9 +22,21 @@ public class EnemieFollowing : MonoBehaviour
 
     void Update()
     {
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        if (CanSeePlayer())
         {
-            IrParaProximoPonto();
+            chasing = true;
+        }
+
+        if (chasing)
+        {
+            agent.SetDestination(player.position);
+        }
+        else
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                IrParaProximoPonto();
+            }
         }
     }
 
@@ -27,5 +46,25 @@ public class EnemieFollowing : MonoBehaviour
 
         agent.SetDestination(pontos[pontoAtual].position);
         pontoAtual = (pontoAtual + 1) % pontos.Length;
+    }
+    bool CanSeePlayer()
+    {
+        Vector3 dir = player.position - transform.position;
+        float distance = dir.magnitude;
+
+        if (distance > viewDistance)
+            return false;
+
+        float angle = Vector3.Angle(transform.forward, dir);
+        if (angle > viewAngle / 2)
+            return false;
+
+        if (Physics.Linecast(transform.position + Vector3.up, player.position, out RaycastHit hit))
+        {
+            if (hit.transform == player)
+                return true;
+        }
+
+        return false;
     }
 }
